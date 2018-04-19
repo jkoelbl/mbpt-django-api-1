25a8644b422572c7a2c5b2760a20974cceb0def3
Nestor Zepeda <NestorDZepeda@gmail.com>
2018-04-18 21:31:14 -0700

    Created new template for the challenges portion of the admin panel. Template now visualizes challenge data based on difficulty and frequency

--- api/challenges/admin.py
+++ api/challenges/admin.py
@@ -1,9 +1,46 @@
 from django.contrib import admin
+from django.db.models import Count, Min, Max
+
 from api.challenges.models import Challenge
 
 
 class ChallengeAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/challenge_admin.html'
     pass
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(request, extra_context=extra_context)
+
+        try:
+            qs = response.context_data['cl'].queryset
+        except(AttributeError, KeyError):
+            return response
+
+        dFrequency = Challenge.objects.values('difficulty').distinct().annotate(frequency=Count('difficulty'))
+        difficulty_range = dFrequency.aggregate(low=Min('difficulty'), max=Max('difficulty'))
+
+        high = difficulty_range.get('max', 0)
+        low = difficulty_range.get('low', 0)
+
+        response.context_data['challenge_difficulty_frequency'] = [{
+            'period': x['difficulty'],
+            'total': int(x['frequency']) or 0,
+            'bar_height': self.get_bar_height(high, low, x['frequency'])}
+
+            for x in dFrequency]
+
+        response.context_data['summary'] = list(
+            qs.values('title', 'difficulty')
+            .order_by('-difficulty')
+        )
+
+        return response
+
+    def get_bar_height(self, high, low, frequency):
+            if frequency == 0:
+                return 0
+            else:
+                return ((frequency - low) / (high - low)) * 100
 
 
 admin.site.register(Challenge, ChallengeAdmin)
