7ab58a9495270e6741525061c45661abbf9253af
Jordan Koelbl <jkoelbl@csumb.edu>
2018-04-25 21:58:28 -0700

    altered endpoints for discussions/comments relating to given profile, updated tests

--- api/profiles/urls.py
+++ api/profiles/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from django.urls import path
 
+from api.discussions.views import DiscussionProfile, CommentProfile
 from api.profiles.views import ProfileDetail
 
 urlpatterns = [
-    url(r'^', ProfileDetail.as_view())
+    path(r'^', ProfileDetail.as_view()),
+    url(r'^discussion/$', DiscussionProfile.as_view()),
+    url(r'^comment/$', CommentProfile.as_view()),
 ]
